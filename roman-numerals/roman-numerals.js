2a4cd9d6e52c5b1fe8140314c5059795b8e28599
Antonio Proenca <antonio.proenca@capco.com>
2019-04-29 20:14:22 -0300

    Adding two more challenges resolutions.

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -1,1 +1,29 @@
-export function 
+export const toRoman = (number) => {
+    let huns = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    let tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    let units = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    let response = "";
+
+    while (number >= 1000) {
+        response += "M"
+        number -= 1000;
+    }
+
+    let size = number.toString().length;
+
+    if (size >= 3) {
+        response += huns[parseInt(number/100)];
+        number = number % 100;
+        response += tens[parseInt(number/10)];
+        number = number % 10;
+        response += units[number];
+    } else if (size == 2) {
+        response += tens[parseInt(number/10)];
+        number = number % 10;
+        response += units[number];
+    } else {
+        response += units[number];
+    }
+
+    return response;
+} 
