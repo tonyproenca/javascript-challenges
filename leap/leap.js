d95b5ca4e628014d4b5b8840ebd00489a80bf276
Antonio Proenca <antonio.proenca@capco.com>
2019-04-29 20:39:23 -0300

    Adding leap challenge.

---
+++ leap/leap.js
@@ -0,0 +1,9 @@
+export const isLeap = (year) => {
+    if (year % 4 == 0) {
+        if (year % 100 == 0 && year % 400 != 0) {
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
